e3ea1f5201372a23da2d5c56d5351dc07bde711e
nsnod <nsnod001@ucr.edu>
2024-08-24 16:13:26 -0700

    links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,19 @@ function App() {
         <a href="https://www.spotify.com" target="_blank" rel="noopener noreferrer">
           <img className="icon" src="spotify.png" alt="Spotify" />
         </a>
-        <a href="https://www.soundcloud.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://soundcloud.com/mistcreep-259157587" target="_blank" rel="noopener noreferrer">
           <img className="icon" src="soundcloud.png" alt="SoundCloud" />
         </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/mistcreeep/" target="_blank" rel="noopener noreferrer">
           <img className="icon" src="insta.png" alt="Instagram" />
         </a>
-        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.youtube.com/@mistcreeep" target="_blank" rel="noopener noreferrer">
           <img className="icon" src="youtube.png" alt="YouTube" />
         </a>
         <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
           <img className="icon" src="linkedin.png" alt="LinkedIn" />
         </a>
-        <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.tiktok.com/@mistcreep" target="_blank" rel="noopener noreferrer">
           <img className="icon" src="tiktok.png" alt="TikTok" />
         </a>
       </div>
